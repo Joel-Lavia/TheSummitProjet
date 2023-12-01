fdb1cae804df3bbeef880ec46c150029bac41c56
Joel-Lavia <joeldisidi8@gmail.com>
2023-12-01 19:58:35 +0100

    integration de la section experience

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Navigate, useNavigate } from "react-router-dom";
 import Header from "../Components/Header";
 import Section from "../Components/Section";
+import SecondSection from "../Components/SecondSection";
 function Home() {
   const navigate = useNavigate();
   const handleClick = (path) => {
@@ -10,6 +11,7 @@ function Home() {
     <div>
       <Header />
       <Section />
+      <SecondSection />
     </div>
   );
 }
