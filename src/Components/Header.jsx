9230954d72fc9477337ac387a5d3d03f8d3e494d
Joel-Lavia <joeldisidi8@gmail.com>
2023-12-01 18:40:59 +0100

    integration du composant header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,13 @@
+import { NavLink } from "react-router-dom";
 function Header() {
 return(
 <header>
-
+<ul>
+    <li><NavLink to=""></NavLink></li>
+    <li><NavLink to=""></NavLink></li>
+    <li><NavLink to=""></NavLink></li>
+    <li><NavLink to=""></NavLink></li>
+</ul>
 </header>
 );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter,RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
-import Home from './pages/Pages.jsx'
+import Home from './pages/Home.jsx'
 
 const router  = createBrowserRouter([
   {
